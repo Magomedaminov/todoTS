b38f5a768e9c1b2d966a2890a33a55c537f84e46
Denis <magomedaminovd@gmail.com>
2023-01-15 20:54:16 +0300

    origin

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/todoList';
 
 function App() {
+
+  let tasks1 = [
+    { id: 1, title: "CSS$html", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "React", isDone: false }
+  ]
+
+  let tasks2 = [
+    { id: 1, title: "Terminator", isDone: true },
+    { id: 2, title: "XXX", isDone: true },
+    { id: 3, title: "Jentelments of fortune", isDone: false }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList title="learn react" tasks={tasks1}/>
+      <TodoList title="Moves" tasks={tasks2}/>
     </div>
   );
 }
+
+
 
 export default App;
